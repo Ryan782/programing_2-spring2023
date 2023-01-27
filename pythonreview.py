ea1143ae40d70ba7e3f4f0d692a4cec5459696b4
Ryan Kershner <rkershner@fwparker.org>
2023-01-27 10:20:38 -0600

    initial commit

---
+++ pythonreview.py
@@ -0,0 +1,29 @@
+
+
+for i in range (100000):
+	i = i*.5
+	print(i)
+	i*2
+
+
+
+'''
+user_name = int(input(("What is your fav number ")))
+user_name2 = int(input(("What is your other fav number ")))
+
+
+number = user_name2+user_name
+if number > 1:
+	print("Thats Way To Big")
+elif number <= 1:
+	print('wow ' + str(number) + ' is just right')
+else:
+	print('Stawp it dawwg')
+
+
+#Cash money dawg 
+for (i) in range(number):
+	print(i)
+'''
+
+
---
+++ starter-app/firstapp/__int__.py
@@ -0,0 +1,4 @@
+user_name = input("What is your fav number")
+user_name2 = input("What is your other fav number")
+number = sum(int(user_name2+user_name))
+print(int(number))
